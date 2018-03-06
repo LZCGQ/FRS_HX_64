66eb583e59a22b6cfb24d0a83eeddabdd7acaf0b
pkj <1152309845@qq.com>
2018-03-06 16:31:30 +0800

    base64 addkey

--- FRSServerHttpWebSocket/Service/VerifyingService.cs
+++ FRSServerHttpWebSocket/Service/VerifyingService.cs
@@ -44,8 +44,10 @@ namespace FRSServerHttp.Service
             if (request.RestConvention == "0")
             {
                 Log.Debug("比较图片");
-                //http://127.0.0.1:8080/v1/verify/0
-                VerifyOneVsOne verify = VerifyOneVsOne.CreateInstanceFromJSON(request.PostParams);
+                //http://127.0.0.1:8080/v1/verify/0      
+                //VerifyOneVsOne verify = VerifyOneVsOne.CreateInstanceFromJSON(request.PostParams);
+                string result = Base64Decode(request.PostParams);
+                VerifyOneVsOne verify = VerifyOneVsOne.CreateInstanceFromJSON(result);
                 if (verify != null)
                 {
                     Bitmap Bitmapsrc = Base64ToImage(verify.PicSrc);
@@ -74,7 +76,9 @@ namespace FRSServerHttp.Service
             else
             {
                 Log.Debug("查找图片");
-                VerifyOneVsN verify = VerifyOneVsN.CreateInstanceFromJSON(request.PostParams);
+                //VerifyOneVsN verify = VerifyOneVsN.CreateInstanceFromJSON(request.PostParams);
+                string result = Base64Decode(request.PostParams);
+                VerifyOneVsN verify = VerifyOneVsN.CreateInstanceFromJSON(result);
                 if (verify != null)
                 {
                     int DatasetId = Convert.ToInt32(request.RestConvention);
@@ -147,6 +151,71 @@ namespace FRSServerHttp.Service
                 stream.Close();
             }
         }
+
+
+
+        /// <summary>
+        /// Base64加密，采用utf8编码方式加密
+        /// </summary>
+        /// <param name="source">待加密的明文</param>
+        /// <returns>加密后的字符串</returns>
+        public static string Base64Encode(string source)
+        {
+            return Base64Encode(Encoding.UTF8, source);
+        }
+
+        /// <summary>
+        /// Base64加密
+        /// </summary>
+        /// <param name="encodeType">加密采用的编码方式</param>
+        /// <param name="source">待加密的明文</param>
+        /// <returns></returns>
+        public static string Base64Encode(Encoding encodeType, string source)
+        {
+            string encode = string.Empty;
+            byte[] bytes = encodeType.GetBytes(source);
+            try
+            {
+                encode = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                encode = source;
+            }
+            return encode;
+        }
+
+        /// <summary>
+        /// Base64解密，采用utf8编码方式解密
+        /// </summary>
+        /// <param name="result">待解密的密文</param>
+        /// <returns>解密后的字符串</returns>
+        public static string Base64Decode(string result)
+        {
+            return Base64Decode(Encoding.UTF8, result);
+        }
+
+        /// <summary>
+        /// Base64解密
+        /// </summary>
+        /// <param name="encodeType">解密采用的编码方式，注意和加密时采用的方式一致</param>
+        /// <param name="result">待解密的密文</param>
+        /// <returns>解密后的字符串</returns>
+        public static string Base64Decode(Encoding encodeType, string result)
+        {
+            string decode = string.Empty;
+            byte[] bytes = Convert.FromBase64String(result);
+            try
+            {
+                decode = encodeType.GetString(bytes);
+            }
+            catch
+            {
+                decode = result;
+            }
+            return decode;
+        }
+        
 
     }
 }
